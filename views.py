35bfe5f0042ce530ca987b05c7c8b9916cc3f212
Shigenmaki <109222327+Makisygen@users.noreply.github.com>
2022-07-13 22:34:37 +0900

    Add files via upload

--- views.py
+++ views.py
@@ -1,11 +1,30 @@
 
 from flask import Flask, render_template,request, redirect
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///Test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO']=True
 
 @app.route('/')
 def hello():
     return "Hello Test"
+
+db = SQLAlchemy(app)
+
+class Movie(db.Model):
+    __tablename__ = 'Movie'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text)
+    category = db.Column(db.Text)
+    price = db.Column(db.Integer)
+
+
+@app.before_first_request
+def init():
+    db.create_all()
 
 
 if __name__ == '__main__':
